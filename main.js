3852119f8826130bd43da62f851a053023023415
Ubuntu <ubuntu@ip-172-31-91-35.ec2.internal>
2019-07-14 15:43:07 +0000

    Add TimeOut function

--- main.js
+++ main.js
@@ -10,10 +10,13 @@ function setData(jsonData) {
 }
 
 xhr.onreadystatechange = function() {
+    console.log(this.readyState);
      if (this.readyState == 4 && this.status == 200) {
         setData(JSON.parse(this.responseText));
 
     }
 };
 
-console.log(data);
+setTimeout(function() {
+    console.log(data);
+}, 500);
